65377a83116547a5ed940e9de421b1b48498fec7
JulyTAriza <jariza28@gmail.com>
2025-10-26 00:03:51 -0500

    Restaurando config de next

--- .history/client/next.config_20251025224109.mjs
+++ .history/client/next.config_20251025224109.mjs
@@ -5,7 +5,14 @@ const nextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+  // Deshabilitar cache pesado de Webpack
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.cache = false;
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
